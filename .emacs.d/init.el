7b2c456657c331d9df910d45a8309cb7d4cc3ecf
Jungseung Yang <xyloon@gmail.com>
2019-03-19 15:02:03 +0900

    Adding pytest option "-s" to enable log

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -524,6 +524,7 @@
  '(elpy-modules
    (quote
     (elpy-module-company elpy-module-eldoc elpy-module-flymake elpy-module-pyvenv elpy-module-highlight-indentation elpy-module-yasnippet elpy-module-django elpy-module-autodoc elpy-module-sane-defaults)))
+ '(elpy-test-pytest-runner-command (quote ("py.test" "-s")))
  '(elpy-test-runner (quote elpy-test-pytest-runner))
  '(package-selected-packages
    (quote
